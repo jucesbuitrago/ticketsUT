b27c7f1ec576eef3ed8ac757f937544bd8eb3637
Jucesbuitrago <cesarbuitrago78@gmail.com>
2024-10-21 22:09:32 -0500

    Mockup

---
+++ Mockup/data/story.js
@@ -0,0 +1,395 @@
+var story = {
+ "docName": "Proyecto Ut (Copy)",
+ "docPath": "P_P_P",
+ "docVersion": 100000001,
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "fileType": "png",
+ "disableInteractions": false,
+ "highlightHotspot": true,
+ "highlightAllHotspots": true,
+ "hideGallery": false,
+ "galleryPageColorsEnabled": true,
+ "galleryDecorNodesEnabled": true,
+ "zoomEnabled": true,
+ "cloud": false,
+ "singleFile": false,
+ "title": "Proyecto Ut (Copy)",
+ "layersExist": true,
+ "pages": [
+  {
+   "id": "32:253",
+   "groupIndex": 0,
+   "title": "Apps",
+   "image": "apps.png",
+   "index": 0,
+   "width": 414,
+   "height": 896,
+   "x": -720,
+   "y": -116,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Rectangle 329",
+     "rect": {
+      "x": 118,
+      "y": 592,
+      "width": 179,
+      "height": 43
+     },
+     "index": 0,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 0,
+       "frameIndex": 1,
+       "transAnimType": 3,
+       "transAnimDuration": 0.30000001192092896,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "35:139",
+   "groupIndex": 0,
+   "title": "Login",
+   "image": "login.png",
+   "index": 1,
+   "width": 414,
+   "height": 896,
+   "x": -266,
+   "y": -116,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Rectangle 329",
+     "rect": {
+      "x": 119,
+      "y": 499,
+      "width": 173,
+      "height": 43
+     },
+     "index": 1,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 1,
+       "frameIndex": 2,
+       "transAnimType": 5,
+       "transAnimDuration": 0.30000001192092896,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "dashicons:arrow-up-alt2",
+     "rect": {
+      "x": 186,
+      "y": 797,
+      "width": 42,
+      "height": 42
+     },
+     "index": 2,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 1,
+       "frameIndex": 0,
+       "transAnimType": 0,
+       "transAnimDuration": 0.30000001192092896,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "3:5",
+   "groupIndex": 0,
+   "title": "Homepage",
+   "image": "homepage.png",
+   "index": 2,
+   "width": 414,
+   "height": 896,
+   "x": 249,
+   "y": -116,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "2021_10_06_09_10_IMG_9181-removebg-preview 1",
+     "rect": {
+      "x": 326,
+      "y": 53,
+      "width": 52,
+      "height": 70.04081726074219
+     },
+     "index": 3,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 6,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Plus circle",
+     "rect": {
+      "x": 181,
+      "y": 644,
+      "width": 52,
+      "height": 52
+     },
+     "index": 4,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 3,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "21:110",
+   "groupIndex": 0,
+   "title": "Details Location",
+   "image": "details-location.png",
+   "index": 3,
+   "width": 414,
+   "height": 896,
+   "x": 703,
+   "y": -116,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Group 11",
+     "rect": {
+      "x": 27,
+      "y": 54,
+      "width": 52.39616775512695,
+      "height": 50
+     },
+     "index": 5,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 0.30000001192092896,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "homepage-2 2",
+     "rect": {
+      "x": 56,
+      "y": 839.5,
+      "width": 30,
+      "height": 30
+     },
+     "index": 6,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Button / primary",
+     "rect": {
+      "x": 20,
+      "y": 542,
+      "width": 353,
+      "height": 56
+     },
+     "index": 7,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 4,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "2209:4508",
+   "groupIndex": 0,
+   "title": "Loading Ticket",
+   "image": "loading-ticket.png",
+   "index": 4,
+   "width": 414,
+   "height": 896,
+   "x": 1221,
+   "y": -116,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Loading Ticket",
+     "rect": {
+      "x": 0,
+      "y": 0,
+      "width": 414,
+      "height": 896
+     },
+     "index": 8,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "AFTER_TIMEOUT",
+       "triggerTimeout": 800,
+       "srcPageIndex": 4,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 0.30000001192092896,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "2212:4932",
+   "groupIndex": 0,
+   "title": "Codigo QR",
+   "image": "codigo-qr.png",
+   "index": 5,
+   "width": 414,
+   "height": 896,
+   "x": 1776,
+   "y": -116,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "homepage-2 2",
+     "rect": {
+      "x": 56,
+      "y": 839.5,
+      "width": 30,
+      "height": 30
+     },
+     "index": 9,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 5,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "16:34",
+   "groupIndex": 0,
+   "title": "My Account",
+   "image": "my-account.png",
+   "index": 6,
+   "width": 424,
+   "height": 896,
+   "x": 2330,
+   "y": -116,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "homepage-2 2",
+     "rect": {
+      "x": 61,
+      "y": 839.5,
+      "width": 30,
+      "height": 30
+     },
+     "index": 10,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 6,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  }
+ ],
+ "startPageIndex": 0,
+ "totalImages": 7,
+ "groups": [
+  {
+   "id": "0:1",
+   "index": 0,
+   "name": "Page 1",
+   "backColor": "#E5E5E5"
+  }
+ ]
+}
